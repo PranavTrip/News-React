ba648fd897e0a197aa7baf7575b597ecc886e519
Pranav <pranavtri00@gmail.com>
2022-05-10 21:57:36 +0530

    News with sample output

--- src/components/NewsComponent.jsx
+++ src/components/NewsComponent.jsx
@@ -2,20 +2,22 @@ import React, { Component } from "react";
 
 export default class NewsComponent extends Component {
   render() {
-      let {title,description,urlToImage}=this.props;
+      let {title,description,urlToImage,url}=this.props;
     return (
       <>
+        <div className="my-4">
         <div className="card" style={{width: "18rem"}}>
-          <img src={urlToImage} className="card-img-top" alt="..." />
+          <img src={urlToImage} className="card-img-top " alt="Image Not Found" style={{height:"10rem"}}/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">
               {description}
             </p>
-            <a href="#" className="btn btn-primary">
+            <a href={url} target="_blank" className="btn btn-sm btn-primary">
               Read More
             </a>
           </div>
+        </div>
         </div>
       </>
     );
